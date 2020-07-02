3be4c829bba9c7b6eea5a52281019523d6db6f28
Piotr Wroblewski <wroblewskipiotr89@gmail.com>
2020-07-02 14:09:24 +0200

    created empty project

---
+++ src/main/java/com/decerto/recruit82513/Recruit82513Application.java
@@ -0,0 +1,13 @@
+package com.decerto.recruit82513;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recruit82513Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recruit82513Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/decerto/recruit82513/Recruit82513ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decerto.recruit82513;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recruit82513ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
